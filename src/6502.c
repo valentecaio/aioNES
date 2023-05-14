4a4ae1c122647fba25d8458d96da053690f696aa
valentecaio <cvalente.aio@gmail.com>
2023-05-14 00:30:00 -0300

    replace unsigned char by uint8_t and unsigned short by uint16_t

--- src/6502.c
+++ src/6502.c
@@ -1,187 +1,187 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-#include "libretro/libretro.h"
-#include "cartridge.h"
 #include "6502.h"
 
-
-void set_flags(unsigned char result) {
+void set_flags_n_z(uint8_t result) {
     flag_n = (result & 0x80) != 0;
     flag_z = (result == 0);
 }
 
+
 /************************** LDA **************************/
-void c6502_lda_immediate(unsigned char operand) {
+void c6502_lda_immediate(uint8_t operand) {
     reg_a = operand;
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_zero_page(unsigned char addr) {
+void c6502_lda_zero_page(uint8_t addr) {
     reg_a = mem[addr];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_zero_page_x(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_x) & 0xFF;
+void c6502_lda_zero_page_x(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_x) & 0xFF;
     reg_a = mem[zp_addr];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_absolute(unsigned short addr) {
+void c6502_lda_absolute(uint16_t addr) {
     reg_a = mem[addr];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_absolute_x(unsigned short addr) {
+void c6502_lda_absolute_x(uint16_t addr) {
     reg_a = mem[addr + reg_x];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_absolute_y(unsigned short addr) {
+void c6502_lda_absolute_y(uint16_t addr) {
     reg_a = mem[addr + reg_y];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_indirect_x(unsigned short addr) {
-    unsigned char effective_addr_l = mem[(addr + reg_x) & 0xFF];
-    unsigned char effective_addr_h = mem[(addr + reg_x + 1) & 0xFF];
-    unsigned short effective_addr = effective_addr_l + (effective_addr_h << 8);
+void c6502_lda_indirect_x(uint16_t addr) {
+    uint8_t effective_addr_l = mem[(addr + reg_x) & 0xFF];
+    uint8_t effective_addr_h = mem[(addr + reg_x + 1) & 0xFF];
+    uint16_t effective_addr = effective_addr_l + (effective_addr_h << 8);
     reg_a = mem[effective_addr];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
-void c6502_lda_indirect_y(unsigned short addr) {
-    unsigned char effective_addr_l = mem[addr];
-    unsigned char effective_addr_h = mem[(addr + 1) & 0xFF];
-    unsigned short effective_addr = (effective_addr_l + (effective_addr_h << 8)) + reg_y;
+void c6502_lda_indirect_y(uint16_t addr) {
+    uint8_t effective_addr_l = mem[addr];
+    uint8_t effective_addr_h = mem[(addr + 1) & 0xFF];
+    uint16_t effective_addr = (effective_addr_l + (effective_addr_h << 8)) + reg_y;
     reg_a = mem[effective_addr];
-    set_flags(reg_a);
+    set_flags_n_z(reg_a);
 }
 
 
 /************************** LDX **************************/
-void c6502_ldx_immediate(unsigned char operand) {
+void c6502_ldx_immediate(uint8_t operand) {
     reg_x = operand;
-    set_flags(reg_x);
+    set_flags_n_z(reg_x);
 }
 
-void c6502_ldx_zero_page(unsigned char addr) {
+void c6502_ldx_zero_page(uint8_t addr) {
     reg_x = mem[addr];
-    set_flags(reg_x);
+    set_flags_n_z(reg_x);
 }
 
-void c6502_ldx_zero_page_y(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_y) & 0xFF;
+void c6502_ldx_zero_page_y(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_y) & 0xFF;
     reg_x = mem[zp_addr];
-    set_flags(reg_x);
+    set_flags_n_z(reg_x);
 }
 
-void c6502_ldx_absolute(unsigned short addr) {
+void c6502_ldx_absolute(uint16_t addr) {
     reg_x = mem[addr];
-    set_flags(reg_x);
+    set_flags_n_z(reg_x);
 }
 
-void c6502_ldx_absolute_y(unsigned short addr) {
+void c6502_ldx_absolute_y(uint16_t addr) {
     reg_x = mem[addr + reg_y];
-    set_flags(reg_x);
+    set_flags_n_z(reg_x);
 }
 
 
 /************************** LDY **************************/
-void c6502_ldy_immediate(unsigned char operand) {
+void c6502_ldy_immediate(uint8_t operand) {
     reg_y = operand;
-    set_flags(reg_y);
+    set_flags_n_z(reg_y);
 }
 
-void c6502_ldy_zero_page(unsigned char addr) {
+void c6502_ldy_zero_page(uint8_t addr) {
     reg_y = mem[addr];
-    set_flags(reg_y);
+    set_flags_n_z(reg_y);
 }
 
-void c6502_ldy_zero_page_x(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_x) & 0xFF;
+void c6502_ldy_zero_page_x(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_x) & 0xFF;
     reg_y = mem[zp_addr];
-    set_flags(reg_y);
+    set_flags_n_z(reg_y);
 }
 
-void c6502_ldy_absolute(unsigned short addr) {
+void c6502_ldy_absolute(uint16_t addr) {
     reg_y = mem[addr];
-    set_flags(reg_y);
+    set_flags_n_z(reg_y);
 }
 
-void c6502_ldy_absolute_x(unsigned short addr) {
+void c6502_ldy_absolute_x(uint16_t addr) {
     reg_y = mem[addr + reg_x];
-    set_flags(reg_y);
+    set_flags_n_z(reg_y);
 }
 
 
 /************************** STA **************************/
-void c6502_sta_immediate(unsigned char addr) {
+void c6502_sta_immediate(uint8_t addr) {
     mem[addr] = reg_a;
 }
 
-void c6502_sta_zero_page(unsigned char addr) {
+void c6502_sta_zero_page(uint8_t addr) {
     mem[addr] = reg_a;
 }
 
-void c6502_sta_zero_page_x(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_x) & 0xFF;
+void c6502_sta_zero_page_x(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_x) & 0xFF;
     mem[zp_addr] = reg_a;
 }
 
-void c6502_sta_absolute(unsigned short addr) {
+void c6502_sta_absolute(uint16_t addr) {
     mem[addr] = reg_a;
 }
 
-void c6502_sta_absolute_x(unsigned short addr) {
+void c6502_sta_absolute_x(uint16_t addr) {
     mem[addr + reg_x] = reg_a;
 }
 
-void c6502_sta_absolute_y(unsigned short addr) {
+void c6502_sta_absolute_y(uint16_t addr) {
     mem[addr + reg_y] = reg_a;
 }
 
-void c6502_sta_indirect_x(unsigned short addr) {
-    unsigned char effective_addr_l = mem[(addr + reg_x) & 0xFF];
-    unsigned char effective_addr_h = mem[(addr + reg_x + 1) & 0xFF];
-    unsigned short effective_addr = effective_addr_l + (effective_addr_h << 8);
+void c6502_sta_indirect_x(uint16_t addr) {
+    uint8_t effective_addr_l = mem[(addr + reg_x) & 0xFF];
+    uint8_t effective_addr_h = mem[(addr + reg_x + 1) & 0xFF];
+    uint16_t effective_addr = effective_addr_l + (effective_addr_h << 8);
     mem[effective_addr] = reg_a;
 }
 
-void c6502_sta_indirect_y(unsigned short addr) {
-    unsigned char effective_addr_l = mem[addr];
-    unsigned char effective_addr_h = mem[(addr + 1) & 0xFF];
-    unsigned short effective_addr = (effective_addr_l + (effective_addr_h << 8)) + reg_y;
+void c6502_sta_indirect_y(uint16_t addr) {
+    uint8_t effective_addr_l = mem[addr];
+    uint8_t effective_addr_h = mem[(addr + 1) & 0xFF];
+    uint16_t effective_addr = (effective_addr_l + (effective_addr_h << 8)) + reg_y;
     mem[effective_addr] = reg_a;
 }
 
 
 /************************** STX **************************/
-void c6502_stx_zero_page(unsigned char addr) {
+void c6502_stx_zero_page(uint8_t addr) {
     mem[addr] = reg_x;
 }
 
-void c6502_stx_zero_page_y(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_y) & 0xFF;
+void c6502_stx_zero_page_y(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_y) & 0xFF;
     mem[zp_addr] = reg_x;
 }
 
-void c6502_stx_absolute(unsigned short addr) {
+void c6502_stx_absolute(uint16_t addr) {
     mem[addr] = reg_x;
 }
 
 
 /************************** STY **************************/
-void c6502_sty_zero_page(unsigned char addr) {
+void c6502_sty_zero_page(uint8_t addr) {
     mem[addr] = reg_y;
 }
 
-void c6502_sty_zero_page_x(unsigned char addr) {
-    unsigned char zp_addr = (addr + reg_x) & 0xFF;
+void c6502_sty_zero_page_x(uint8_t addr) {
+    uint8_t zp_addr = (addr + reg_x) & 0xFF;
     mem[zp_addr] = reg_y;
 }
 
-void c6502_sty_absolute(unsigned short addr) {
+void c6502_sty_absolute(uint16_t addr) {
     mem[addr] = reg_y;
 }
+
 
--- src/6502.h
+++ src/6502.h
@@ -2,6 +2,10 @@
 #define C6502_INSTRUCTIONS_H
 
 #include <stdbool.h>
+#include <stdint.h>
+
+#include "libretro/libretro.h"
+#include "cartridge.h"
 
 /*
 $0000–$07FF   $0800   2 KB internal RAM
@@ -14,16 +18,16 @@ $4000–$4017   $0018   NES APU and I/O registers
 $4018–$401F   $0008   APU and I/O functionality that is normally disabled. See CPU Test Mode.
 $4020–$FFFF   $BFE0   Cartridge space: PRG ROM, PRG RAM, and mapper registers
 */
-unsigned char mem[65536] = {0};
-unsigned char reg_a = 0;           // accumulator
-unsigned char reg_x = 0;
-unsigned char reg_y = 0;
+uint8_t mem[65536] = {0};
+uint8_t reg_a = 0;           // accumulator
+uint8_t reg_x = 0;
+uint8_t reg_y = 0;
 bool flag_n = false; // negative    keeps track if the result in the ALU is negative
 bool flag_z = false; // zero        keeps track if the result of a calculation is zero
 bool flag_v = false; // overflow
 bool flag_c = false; // carry
 
-void set_flags(unsigned char result);
+void set_flags(uint8_t result);
 
 
 /*  Load Accumulator (flags: N,Z)
@@ -36,14 +40,14 @@ void set_flags(unsigned char result);
     LDA ($aa),X     $A1     2   c6502_lda_indirect_x      Indirect, X-Indexed Addressing
     LDA ($aa,Y)     $B1     2   c6502_lda_indirect_y      Indirect Y-Indexed Addressing
 */
-void c6502_lda_immediate(unsigned char operand);
-void c6502_lda_zero_page(unsigned char addr);
-void c6502_lda_zero_page_x(unsigned char addr);
-void c6502_lda_absolute(unsigned short addr);
-void c6502_lda_absolute_x(unsigned short addr);
-void c6502_lda_absolute_y(unsigned short addr);
-void c6502_lda_indirect_x(unsigned short addr);
-void c6502_lda_indirect_y(unsigned short addr);
+void c6502_lda_immediate(uint8_t operand);
+void c6502_lda_zero_page(uint8_t addr);
+void c6502_lda_zero_page_x(uint8_t addr);
+void c6502_lda_absolute(uint16_t addr);
+void c6502_lda_absolute_x(uint16_t addr);
+void c6502_lda_absolute_y(uint16_t addr);
+void c6502_lda_indirect_x(uint16_t addr);
+void c6502_lda_indirect_y(uint16_t addr);
 
 
 /*  Load X Register (flags: N,Z)
@@ -53,11 +57,11 @@ void c6502_lda_indirect_y(unsigned short addr);
     LDX $aaaa       $AE     3   c6502_ldx_absolute
     LDX $aaaa,Y     $BE     3   c6502_ldx_absolute_y
 */
-void c6502_ldx_immediate(unsigned char operand);
-void c6502_ldx_zero_page(unsigned char addr);
-void c6502_ldx_zero_page_y(unsigned char addr);
-void c6502_ldx_absolute(unsigned short addr);
-void c6502_ldx_absolute_y(unsigned short addr);
+void c6502_ldx_immediate(uint8_t operand);
+void c6502_ldx_zero_page(uint8_t addr);
+void c6502_ldx_zero_page_y(uint8_t addr);
+void c6502_ldx_absolute(uint16_t addr);
+void c6502_ldx_absolute_y(uint16_t addr);
 
 
 /*  Load Y Register (flags: N,Z)
@@ -67,11 +71,11 @@ void c6502_ldx_absolute_y(unsigned short addr);
     LDY $aaaa       $AC     3   c6502_ldy_absolute
     LDY $aaaa,X     $BC     3   c6502_ldy_absolute_x
 */
-void c6502_ldy_immediate(unsigned char operand);
-void c6502_ldy_zero_page(unsigned char addr);
-void c6502_ldy_zero_page_x(unsigned char addr);
-void c6502_ldy_absolute(unsigned short addr);
-void c6502_ldy_absolute_x(unsigned short addr);
+void c6502_ldy_immediate(uint8_t operand);
+void c6502_ldy_zero_page(uint8_t addr);
+void c6502_ldy_zero_page_x(uint8_t addr);
+void c6502_ldy_absolute(uint16_t addr);
+void c6502_ldy_absolute_x(uint16_t addr);
 
 
 /*  Store Accumulator (flags: none)
@@ -83,14 +87,14 @@ void c6502_ldy_absolute_x(unsigned short addr);
     STA ($aa,X)     $81     2   c6502_sta_indirect_x
     STA ($aa),Y     $91     2   c6502_sta_indirect_y
 */
-void c6502_sta_zero_page(unsigned char addr);
-void c6502_sta_zero_page_x(unsigned char addr);
-void c6502_sta_zero_page_y(unsigned char addr);
-void c6502_sta_absolute(unsigned short addr);
-void c6502_sta_absolute_x(unsigned short addr);
-void c6502_sta_absolute_y(unsigned short addr);
-void c6502_sta_indirect_x(unsigned short addr);
-void c6502_sta_indirect_y(unsigned short addr);
+void c6502_sta_zero_page(uint8_t addr);
+void c6502_sta_zero_page_x(uint8_t addr);
+void c6502_sta_zero_page_y(uint8_t addr);
+void c6502_sta_absolute(uint16_t addr);
+void c6502_sta_absolute_x(uint16_t addr);
+void c6502_sta_absolute_y(uint16_t addr);
+void c6502_sta_indirect_x(uint16_t addr);
+void c6502_sta_indirect_y(uint16_t addr);
 
 
 /*  Store X Register (flags: none)
@@ -98,9 +102,9 @@ void c6502_sta_indirect_y(unsigned short addr);
     STX $aa,Y   $96     2   c6502_stx_zero_page_y
     STX $aaaa   $8E     3   c6502_stx_absolute
 */
-void c6502_stx_zero_page(unsigned char addr);
-void c6502_stx_zero_page_y(unsigned char addr);
-void c6502_stx_absolute(unsigned short addr);
+void c6502_stx_zero_page(uint8_t addr);
+void c6502_stx_zero_page_y(uint8_t addr);
+void c6502_stx_absolute(uint16_t addr);
 
 
 /*  Store Y Register (flags: none)
@@ -108,8 +112,9 @@ void c6502_stx_absolute(unsigned short addr);
     STY $aa,X   $94     2   c6502_sty_zero_page_x
     STY $aaaa   $8C     3   c6502_sty_absolute
 */
-void c6502_sty_zero_page(unsigned char addr);
-void c6502_sty_zero_page_x(unsigned char addr);
-void c6502_sty_absolute(unsigned short addr);
+void c6502_sty_zero_page(uint8_t addr);
+void c6502_sty_zero_page_x(uint8_t addr);
+void c6502_sty_absolute(uint16_t addr);
+
 
 #endif /* C6502_INSTRUCTIONS_H */
